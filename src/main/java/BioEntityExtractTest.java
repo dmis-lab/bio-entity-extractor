e79aba30c9702828fa1558819f5994dcbe4933a3
donghyeon_kim <donghyeon@korea.ac.kr>
2018-11-26 13:17:29 +0900

    Move to github

---
+++ src/main/java/BioEntityExtractTest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import org._dmis.object.*;
+
+public class BioEntityExtractTest {
+    public static void main(String[] args) {
+        String dictPath = "src/main/resources/dictionary.txt";
+        BioEntityExtractor bee = new BioEntityExtractor(dictPath);
+
+        String text = "Lapatinib inhibits cell growth in cell lines.";
+
+        HashSet<BioEntityInfo> entitySet = bee.extractEntities(text);
+
+        for(BioEntityInfo entity : entitySet) {
+            System.out.println("ID: " + entity.getIdStr() + ", Name: " + entity.getName());
+
+            ArrayList<BioEntityType> types = entity.getTypes();
+
+            for(BioEntityType bet : types) {
+                System.out.print("\t" + bet.getName4EndUser());
+            }
+            System.out.println();
+        }
+    }
+}
